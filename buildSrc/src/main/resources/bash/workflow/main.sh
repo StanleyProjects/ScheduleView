dcc3a94589450905c22cf398b3cd0322d012e641
Stanley Wintergreen <kepocnhh@gmail.com>
2021-03-16 21:55:21 +0300

    wip | github workflow pipeline main...

--- buildSrc/src/main/resources/bash/workflow/main.sh
+++ buildSrc/src/main/resources/bash/workflow/main.sh
@@ -2,9 +2,24 @@
 
 echo "main start..."
 
+ERROR_CODE_COMMON_FILE=11
+ERROR_CODE_VCS=21
+
 export WORKFLOW=$RESOURCES_PATH/bash/workflow
 
-. $WORKFLOW/vcs.sh || exit 1
+FILE="$WORKFLOW/vcs.sh"
+if [ ! -f "$FILE" ]; then
+ echo "Version control system file \"$FILE\" error $CODE!"
+ exit $ERROR_CODE_COMMON_FILE
+fi
+
+. $FILE
+CODE=$?
+if test $CODE -ne 0; then
+ echo "Version control system error $CODE!"
+ exit $ERROR_CODE_VCS
+fi
+
 exit 1 # todo
 
 echo "main success"
